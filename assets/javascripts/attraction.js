467f9a4b3b95b4dbebd07e624ea2196b61d20133
Yaniv Leviathan <leviathan@leviathan-macbookpro3.roam.corp.google.com>
2020-01-11 02:18:49 -0500

    .

--- assets/javascripts/attraction.js
+++ assets/javascripts/attraction.js
@@ -1,6 +1,9 @@
 var canvas = document.getElementById('zlata');
 var context = canvas.getContext('2d');
 
+// var max_accel = 50;
+// var min_accel = 1;
+
 const CHARS = new Map([
   ['Z', '*****   *   *   *   *****'],
   ['L', '*    *    *    *    *****'],
@@ -30,14 +33,30 @@ class Vec2 {
     this.y = y;
   }
 
+  get len2() {
+    return this.x * this.x + this.y * this.y;
+  }
+
   get len() {
-    return Math.sqrt(this.x * this.x + this.y * this.y);
+    return Math.sqrt(this.len2);
   }
 
   set len(new_len) {
     let frac = new_len / this.len;
     this.x *= frac;
     this.y *= frac;
+  }
+
+  add(v) {
+    return new Vec2(this.x + v.x, this.y + v.y);
+  }
+
+  sub(v) {
+    return new Vec2(this.x - v.x, this.y - v.y);
+  }
+
+  mul(a) {
+    return new Vec2(this.x * a, this.y * a);
   }
 }
 
@@ -97,37 +116,75 @@ function toColorString(rgb) {
 const COLORS = ['red', 'blue', 'yellow', 'green', 'orange',
   'purple', 'pink', 'white', 'cyan', 'magenta'];
 
+const prototypes = [
+  { 'color': 'rgb(255, 150, 150)', 'min_accel': 1, 'max_accel': 50, 'verlet': false, 'max_idle_vel': 500 },
+  { 'color': 'rgb(150, 255, 150)', 'min_accel': 1, 'max_accel': 50, 'verlet': false, 'max_idle_vel': 500 },
+  { 'color': 'rgb(255, 255, 150)', 'min_accel': 0, 'max_accel': 50, 'verlet': false, 'max_idle_vel': 500 },
+  { 'color': 'rgb(150, 150, 255)', 'min_accel': 10, 'max_accel': 30, 'verlet': false, 'max_idle_vel': 50 },
+  { 'color': 'rgb(255, 150, 255)', 'min_accel': 2, 'max_accel': 20, 'verlet': false, 'max_idle_vel': 500 },
+];
 
 class Particle {
   collide() {
     if (this.box.left < 0) {
       this.vel.x = Math.abs(this.vel.x);
+      if (this.prev_pos.x > this.box.pos.x) {
+        this.prev_pos.x = this.box.pos.x + (this.box.pos.x - this.prev_pos.x);
+      }
     }
     if (this.box.right > canvas.width) {
       this.vel.x = -Math.abs(this.vel.x);
+      if (this.prev_pos.x < this.box.pos.x) {
+        this.prev_pos.x = this.box.pos.x + (this.box.pos.x - this.prev_pos.x);
+      }
     }
     if (this.box.top < 0) {
       this.vel.y = Math.abs(this.vel.y);
+      if (this.prev_pos.y > this.box.pos.y) {
+        this.prev_pos.y = this.box.pos.y + (this.box.pos.y - this.prev_pos.y);
+      }
     }
     if (this.box.bottom > canvas.height) {
       this.vel.y = -Math.abs(this.vel.y);
+      if (this.prev_pos.y < this.box.pos.y) {
+        this.prev_pos.y = this.box.pos.y + (this.box.pos.y - this.prev_pos.y);
+      }
     }
   }
 
   set attractor(attractor) {
     this._attractor = attractor;
-    this.vel.len = 500; // xxx
-    // this.color = COLORS[Math.floor(COLORS.length * (Math.random() * 100 + attractor.x) / 800)];
+    if (attractor == null) {
+      this.vel.len = this.max_idle_vel; // xxx
+      let v = this.box.pos.sub(this.prev_pos);
+      v.len = this.max_idle_vel;
+      this.prev_pos = this.box.pos.sub(v.mul(1 / 60));
+    }
+    // // this.color = COLORS[Math.floor(COLORS.length * (Math.random() * 100 + attractor.x) / 800)];
   }
 
   attractToward(point) {
-    var org_len = this.vel.len;
-    var accel = new Vec2(point.x - this.box.pos.x,
-      point.y - this.box.pos.y);
-    this.vel.x += accel.x;
-    this.vel.y += accel.y;
-    this.vel.len = org_len;
-    this.vel.len = Math.min(org_len, accel.len * 20); // xxx
+    // var org_len = this.vel.len;
+    this.force = point.sub(this.box.pos);
+    this.force.len = Math.max(Math.min(this.force.len, this.max_accel), this.min_accel);
+    // accel.len = Math.min(accel.len, max_accel);
+    // accel.len = max_accel / (accel.len2 + 1);
+
+    // this.vel = this.vel.add(accel);
+    // // this.vel.len = Math.min(this.vel.len, max_vel);
+    // this.vel.len = Math.min(org_len, accel.len * 20); // xxx
+    // // this.vel.len = Math.min(100, accel.len * 20); // xxx
+    // // this.vel.len = org_len; // xxx
+    // // this.vel.len = 100;
+
+    // let v = this.box.pos.sub(this.prev_pos).mul(60);
+    // org_len = v.len;
+    // v = v.add(accel);
+    // v.len = Math.min(org_len, accel.len * 20); // xxx
+    // // v.len = Math.min(v.len, max_vel / 60);
+    // // v.len = org_len; // xxx
+    // // v.len = 100;
+    // this.prev_pos = this.box.pos.sub(v.mul(1 / 60));
   }
 
   constructor() {
@@ -136,23 +193,63 @@ class Particle {
     this.box.pos.y = Math.random() * canvas.height;
     this.vel = new Vec2(Math.random() - 0.5, Math.random() - 0.5);
     this.vel.len = 100;
-    // this.vel.len = 300;xxx
+    this.force = new Vec2(0, 0);
+    this.prev_pos = this.box.pos.sub(this.vel.mul(1 / 60));
+    // this.prev_delta_time = 1;
     this._attractor = null;
-    this.color = COLORS[Math.floor(Math.random() * COLORS.length)];
+
+    let prototype = prototypes[Math.floor(Math.random() * prototypes.length)];
+    this.color = prototype['color'];
+    this.min_accel = prototype['min_accel'];
+    this.max_accel = prototype['max_accel'];
+    this.use_verlet = prototype['verlet'];
+    this.max_idle_vel = prototype['max_idle_vel'];
+
+    // if (Math.random() < 0.5) {
+    //   this.color = 'rgb(255, 100, 100)';
+    //   this.use_verlet = true;
+    // } else {
+    //   this.color = 'rgb(100,100,255)';
+    //   this.use_verlet = false;
+    // }
+    // this.color = COLORS[Math.floor(Math.random() * COLORS.length)];
     // this.color = toColorString([255,
     //                             255,
     //                             Math.floor(Math.random() * 128 + 128)]);
   }
 
   draw(context) {
-    context.fillStyle = this.color;
-    context.fillRect(this.box.left, this.box.top,
-      this.box.width, this.box.height);
+    let px = Math.floor(this.prev_pos.x);
+    let py = Math.floor(this.prev_pos.y);
+    let x = Math.floor(this.box.pos.x);
+    let y = Math.floor(this.box.pos.y);
+    if (px == x && y == py) {
+      context.fillStyle = this.color;
+      context.fillRect(x, y, 2, 2);
+    } else {
+      context.strokeStyle = this.color;
+      context.lineWidth = 2;
+      context.beginPath();
+      context.moveTo(px, py);
+      context.lineTo(x, y);
+      context.stroke();
+    }
   }
 
   update(delta_time) {
-    this.box.pos.x += this.vel.x * delta_time;
-    this.box.pos.y += this.vel.y * delta_time;
+    this.prev_pos = this.box.pos;
+    if (!this.use_verlet) {
+      this.vel = this.vel.mul(0.95).add(this.force);
+      // this.vel = this.force;
+      this.box.pos = this.box.pos.add(this.vel.mul(delta_time));
+    } else {
+      let new_prev_pos = this.box.pos;
+      let v = this.box.pos.sub(this.prev_pos).mul(60).mul(0.95).add(this.force);
+      this.box.pos = this.box.pos.add(v.mul(1 / 60));
+      // this.box.pos = this.box.pos.add(v);
+      this.prev_pos = new_prev_pos;
+      // this.prev_delta_time = delta_time;
+    }
     if (this._attractor) {
       this.attractToward(this._attractor);
     }
